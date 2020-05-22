a34a19d18ba7e1a05a1466d450ed8dee5292aba1
cdelange <casey.delange@gmail.com>
2020-05-21 22:11:41 -0500

    coffe-quiz

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import coffeeBean from './assets/coffee-bean.png'; // with import
+import coffeeBean from '../assets/bean.png';
 
 function Loader() {
   return (
     <div className="row" >
     <div className='loader col-xs-12'>
         <div>
-            <img src="{coffeeBean}" />
+            <img src={coffeeBean} />
         </div>
         <div>
-            <img src="{coffeeBean}" />
+            <img src={coffeeBean} />
         </div>
         <div>
-            <img src="{coffeeBean}" />
+            <img src={coffeeBean} />
         </div>
     </div>
     </div>
